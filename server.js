2e5e0a5cbc493d52cc31b6acd4c8b3055482df9b
Daviles82 <avileshouston@live.com>
2024-03-08 23:26:05 -0600

    installed swagger

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
-const express = require("express");
-
+const express = require('express');
+const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-allow-Headers',
+    'Orgin, X-Requested-With, Content-Type, Accept, Z-Key'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
 app.use('/', require('./routes'));
 
-
-mongodb.initDb((err)=> {
-  if(err){
+mongodb.initDb((err) => {
+  if (err) {
     console.log(err);
+  } else {
+    app.listen(port, () => {
+      console.log(`Database is listening and node Running on port ${port}`);
+    });
   }
-  else{
-    app.listen(port, () => {console.log(`Database is listening and node Running on port ${port}`);});
-  }
-})
-
-
+});
